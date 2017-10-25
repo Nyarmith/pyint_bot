2bee8f90235b9f3b6be563853c819d78bd5eaa4b
nyarmith <powah.serge@gmail.com>
2017-10-25 13:23:32 -0400

    v0.1

--- pint_bot_docker.py
+++ pint_bot_docker.py
@@ -1,18 +1,4 @@
 #!/usr/bin/python3
-
-#basically a 3-level dictionary 
-#    {chat_id1 : 
-#        {list1 : [item1, item2], 
-#        .list1 : [item3, item4], 
-#         list2 : [item1, item2],
-#        .list2 : [item3] , ...}, 
-#     chat_id2
-#        {list1 : [item1, item2], 
-#        .list1 : [item3, item4], 
-#         list2 : [item1, item2],
-#        .list2 : [item3] , ...}, 
-#..}
-
 import telepot
 import pexpect
 import os
@@ -29,9 +15,9 @@ def handle(msg):
 
     if command[:4] == '/run':
         cmd = command[5:]
-        ret = runCmd(cmd)
         session.sendline(cmd)
         session.expect('>>>')
+        print(session.before)
         bot.sendMessage(chat_id, session.before)
 
     elif command[:5] == '/help':
